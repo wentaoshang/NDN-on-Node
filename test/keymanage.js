99057d916b473097bf708138e361f26fc6efaa8b
Wentao Shang <wentaoshang@gmail.com>
2013-07-02 12:29:58 -0700

    Add helper functions to Key object

--- test/keymanage.js
+++ test/keymanage.js
@@ -3,6 +3,8 @@ var Key = require('../build/ndn.js').Key;
 var key = new Key();
 key.fromPemFile('./non.pub', './non.pem');
 
-console.log('Public key DER:\n' + key.publicKeyDer.toString('hex'));
-console.log('Public key PEM:\n' + key.publicKeyPem);
-console.log('Private key PEM:\n' + key.privateKeyPem);
+console.log('Public key DER:\n' + key.publicToDER().toString('hex'));
+console.log('Public key PEM:\n' + key.publicToPEM());
+console.log('Public key ID:\n' + key.getKeyID().toString('hex'));
+console.log('Private key DER:\n' + key.privateToDER().toString('hex'));
+console.log('Private key PEM:\n' + key.privateToPEM());
