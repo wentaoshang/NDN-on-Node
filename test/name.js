03f24b9feb934e8191ba84e57e18a4b58a505ab0
Wentao Shang <wentaoshang@gmail.com>
2013-07-02 00:03:20 -0700

    Fix bug when encoding %00 in name component. Add helper functions to Name prototype.

--- test/name.js
+++ test/name.js
@@ -2,9 +2,27 @@ var NoN = require("../build/ndn.js");
 
 var name = new NoN.Name("/%C1.M.S.localhost/%C1.M.SRV/ccnd/KEY");
 
+console.log("Name for '/%C1.M.S.localhost/%C1.M.SRV/ccnd/KEY':");
 console.log("Buffers:");
 console.log(name.components);
 console.log("URI representation:");
 console.log(name.to_uri());
 console.log('XML representation:');
 console.log(name.to_xml());
+
+
+var key = new NoN.Key();
+key.fromPemFile('./non.pub', './non.pem');
+
+var n1 = new NoN.Name('/wentao.shang/%C1%00/test001');
+n1.appendKeyID(key).appendVersion().appendSegment(0);
+
+console.log("----------------------");
+console.log("Name for the key:");
+console.log("Buffers:");
+console.log(n1.components);
+console.log("URI representation:");
+console.log(n1.to_uri());
+console.log('XML representation:');
+console.log(n1.to_xml());
+
