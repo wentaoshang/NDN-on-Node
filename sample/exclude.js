6794352e220251bdef1f80bd6f5e73c35e7e958a
Wentao Shang <wentaoshang@gmail.com>
2013-07-04 10:11:32 -0700

    Better test case for Exclude.

--- sample/exclude.js
+++ sample/exclude.js
@@ -5,11 +5,20 @@ var i = new NoN.Interest(n);
 i.answerOriginKind = NoN.Interest.ANSWER_NO_CONTENT_STORE;
 i.interestLifetime = 1234;
 
-var seg = new Buffer(2);
-seg[0] = 0;
-seg[1] = 12;
-var filter = new NoN.Exclude(['before', NoN.Exclude.ANY, new Buffer('after', 'ascii'), NoN.Exclude.ANY, seg]);
+// XXX: this filter is meaningless, just for test purpose
+var filter = new NoN.Exclude(['before', NoN.Exclude.ANY, new Buffer('after', 'ascii'), NoN.Exclude.ANY, '%00%10']);
 i.exclude = filter;
 
-console.log('Interest in XML representation:');
+console.log('Interest with random Exclude:');
 console.log(i.to_xml());
+
+// Test Exlucde.matches()
+var filter1 = new NoN.Exclude(['%00%02', NoN.Exclude.ANY, '%00%20']);
+console.log('Meaningful Exclude:');
+console.log(filter1.to_xml());
+
+var comp1 = NoN.Name.stringComponentToBuffer('%00%01');
+var comp2 = NoN.Name.stringComponentToBuffer('%00%0F');
+console.log('Matches:');
+console.log(NoN.Name.toEscapedString(comp1) + ' ? ' + filter1.matches(comp1));
+console.log(NoN.Name.toEscapedString(comp2) + ' ? ' + filter1.matches(comp2));
