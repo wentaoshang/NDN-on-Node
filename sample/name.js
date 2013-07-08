bf5e235beabb8495e3c298846959993fe882c2ea
Wentao Shang <wentaoshang@gmail.com>
2013-07-08 10:00:07 -0700

    Add Name.isPrefixOf() helper function.

--- sample/name.js
+++ sample/name.js
@@ -1,6 +1,6 @@
-var NoN = require("../build/ndn.js");
+var non = require("../build/ndn.js");
 
-var name = new NoN.Name("/%C1.M.S.localhost/%C1.M.SRV/ccnd/KEY");
+var name = new non.Name("/%C1.M.S.localhost/%C1.M.SRV/ccnd/KEY");
 
 console.log("Name for '/%C1.M.S.localhost/%C1.M.SRV/ccnd/KEY':");
 console.log("Buffers:");
@@ -11,10 +11,10 @@ console.log('XML representation:');
 console.log(name.to_xml());
 
 
-var key = new NoN.Key();
+var key = new non.Key();
 key.fromPemFile('./non.pub', './non.pem');
 
-var n1 = new NoN.Name('/wentao.shang/%C1%00/test001');
+var n1 = new non.Name('/wentao.shang/%C1%00/test001');
 n1.appendKeyID(key).appendVersion().appendSegment(0);
 
 console.log("----------------------");
@@ -27,10 +27,10 @@ console.log('XML representation:');
 console.log(n1.to_xml());
 
 console.log("----------------------");
-var n2 = new NoN.Name('/a/b/c.txt/');
+var n2 = new non.Name('/a/b/c.txt/');
 console.log("Original name:");
 console.log(n2.to_uri());
-var n3 = new NoN.Name(n2);
+var n3 = new non.Name(n2);
 console.log("Make a copy:");
 console.log(n3.to_uri());
 n3.appendVersion().appendSegment(0);
@@ -38,3 +38,10 @@ console.log("Make some change:")
 console.log(n3.to_uri());
 console.log("Original name:");
 console.log(n2.to_uri());
+
+
+var n4 = new non.Name('/a/b');
+var n5 = new non.Name('/a/b/c/d');
+
+console.log("----------------------");
+console.log(n4.to_uri() + ' is a prefix of ' + n5.to_uri() + '? ' + n4.isPrefixOf(n5));
