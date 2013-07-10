db69796035a747e11bf0da54acc415c5f54ddbc3
Wentao Shang <wentaoshang@gmail.com>
2013-07-10 14:49:54 -0700

    Fix bug in Exclude constructor and publish to version 0.0.9

--- sample/interest.js
+++ sample/interest.js
@@ -7,3 +7,16 @@ i.interestLifetime = 1234;
 
 console.log('Interest in XML representation:');
 console.log(i.to_xml());
+
+var n1 = new non.Name('/a/b/c.txt');
+var i1 = new non.Interest(n1);
+i1.interestLifetime = 1000;
+i1.childSelector = 1;
+i1.exclude = new non.Exclude(['%00%02', non.Exclude.ANY, '%00%20']);
+console.log(i1.to_xml());
+    
+var name1 = new non.Name('/a/b/c.txt/%00%01');
+var name2 = new non.Name('/a/b/c.txt/%00%0F');
+console.log('Interest matches Name:');
+console.log(name1.to_uri() + ' ? ' + i1.matches_name(name1));
+console.log(name2.to_uri() + ' ? ' + i1.matches_name(name2));
